59110c9a1c8a401441dfeec638db4395bc6851bd
Andreas Lill <andreas.ste.lill@gmail.com>
2021-10-05 14:30:09 +0200

    Changed SettingsDialog to extend BottomSheetDialogFragment instead of DialogFragment.

--- app/src/main/java/com/andlill/jld/app/settings/dialog/SettingsDialog.kt
+++ app/src/main/java/com/andlill/jld/app/settings/dialog/SettingsDialog.kt
@@ -1,30 +1,24 @@
 package com.andlill.jld.app.settings.dialog
 
-import android.app.Dialog
-import android.content.DialogInterface
 import android.os.Bundle
-import android.os.Handler
-import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
-import androidx.core.os.HandlerCompat
-import androidx.fragment.app.DialogFragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.andlill.jld.R
 import com.andlill.jld.app.settings.adapter.SettingsAdapter
 import com.andlill.jld.utils.AppUtils
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import java.util.*
 
-class SettingsDialog(private val title: String, private val settings: List<String>, private val selected: String, private val callback: (String) -> Unit) : DialogFragment() {
+class SettingsDialog(private val title: String, private val settings: List<String>, private val selected: String, private val callback: (String) -> Unit) : BottomSheetDialogFragment() {
 
     private var value: String = ""
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val layout = requireActivity().layoutInflater.inflate(R.layout.dialog_settings, null)
-        val dialog = AlertDialog.Builder(requireContext(), R.style.Theme_MaterialComponents_Dialog_Alert).setView(layout).create()
-        dialog.window?.setGravity(Gravity.BOTTOM)
-        dialog.window?.attributes?.windowAnimations = R.style.DialogAnimationSlide
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val layout = layoutInflater.inflate(R.layout.dialog_settings, container, false)
 
         val settingsAdapter = SettingsAdapter(settings, selected) { result ->
             value = result
@@ -39,7 +33,7 @@ class SettingsDialog(private val title: String, private val settings: List<Strin
             adapter = settingsAdapter
         }
 
-        return dialog
+        return layout
     }
 
     override fun onDestroy() {
