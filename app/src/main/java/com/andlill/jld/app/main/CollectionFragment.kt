600237f4071d9e39e389881f9468bda74fa46230
Andreas Lill <andreas.ste.lill@gmail.com>
2021-10-06 19:15:33 +0200

    Added bottom sheet dialog for creating/importing collection.

--- app/src/main/java/com/andlill/jld/app/main/CollectionFragment.kt
+++ app/src/main/java/com/andlill/jld/app/main/CollectionFragment.kt
@@ -3,10 +3,8 @@ package com.andlill.jld.app.main
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
 import androidx.activity.result.ActivityResult
-import androidx.core.os.HandlerCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,12 +12,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.andlill.jld.R
 import com.andlill.jld.app.collectiondetails.CollectionDetailsActivity
 import com.andlill.jld.app.main.adapter.CollectionAdapter
+import com.andlill.jld.app.main.dialog.CollectionOptionsDialog
 import com.andlill.jld.app.main.dialog.ImportCollectionDialog
 import com.andlill.jld.app.main.dialog.NameCollectionDialog
 import com.andlill.jld.app.shared.ResultActivityFragment
 import com.andlill.jld.app.shared.dialog.RenameCollectionDialog
 import com.andlill.jld.model.Collection
-import com.andlill.jld.utils.ViewAnimation
 import com.google.android.material.progressindicator.LinearProgressIndicator
 import com.google.android.material.snackbar.Snackbar
 
@@ -27,11 +25,7 @@ class CollectionFragment : ResultActivityFragment(R.layout.fragment_collection) 
 
     private lateinit var viewModel: CollectionFragmentViewModel
     private lateinit var collectionAdapter: CollectionAdapter
-
-    private lateinit var scrim: View
     private lateinit var actionButton: View
-    private lateinit var actionButtonNew: View
-    private lateinit var actionButtonImport: View
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -57,41 +51,16 @@ class CollectionFragment : ResultActivityFragment(R.layout.fragment_collection) 
             adapter = collectionAdapter
         }
 
-        scrim = view.findViewById<View>(R.id.scrim).apply { setOnClickListener { hideActionButtonMenu() } }
-        actionButton = view.findViewById<View>(R.id.floating_action_button).apply { setOnClickListener { toggleActionButtonMenu() }}
-        actionButtonNew = view.findViewById<View>(R.id.floating_action_new).apply { setOnClickListener { createCollectionDialog() }}
-        actionButtonImport = view.findViewById<View>(R.id.floating_action_import).apply { setOnClickListener { importCollectionDialog() }}
-    }
-
-    private fun showActionButtonMenu() {
-        scrim.visibility = View.VISIBLE
-        if (actionButton.rotation == 0f) {
-            ViewAnimation.rotate(actionButton, 150, 135f, 1f, 0.5f) { callback ->
-                if (callback == ViewAnimation.State.Start) {
-                    ViewAnimation.fadeIn(actionButtonNew, 150)
-                    HandlerCompat.postDelayed(Handler(), { ViewAnimation.fadeIn(actionButtonImport, 150) }, null, 50)
-                }
+        actionButton = view.findViewById<View>(R.id.floating_action_button).apply {
+            setOnClickListener {
+                CollectionOptionsDialog { action ->
+                    when (action) {
+                        CollectionOptionsDialog.Action.Import -> importCollectionDialog()
+                        CollectionOptionsDialog.Action.New -> newCollectionDialog()
+                    }
+                }.show(requireActivity().supportFragmentManager, CollectionOptionsDialog::class.simpleName)
             }
         }
-    }
-
-    private fun hideActionButtonMenu() {
-        scrim.visibility = View.INVISIBLE
-        if (actionButton.rotation > 0f) {
-            ViewAnimation.rotate(actionButton, 150, 0f, 0.5f, 1f) { callback ->
-                if (callback == ViewAnimation.State.Start) {
-                    HandlerCompat.postDelayed(Handler(), { ViewAnimation.fadeOut(actionButtonNew, 150) }, null, 50)
-                    ViewAnimation.fadeOut(actionButtonImport, 150)
-                }
-            }
-        }
-    }
-
-    private fun toggleActionButtonMenu() {
-        if (actionButton.rotation == 0f)
-            showActionButtonMenu()
-        else
-            hideActionButtonMenu()
     }
 
     override fun onStart() {
@@ -134,15 +103,13 @@ class CollectionFragment : ResultActivityFragment(R.layout.fragment_collection) 
     }
 
     private fun importCollectionDialog() {
-        hideActionButtonMenu()
         ImportCollectionDialog { collection ->
             collection.id = 0
             viewModel.addCollection(requireContext(), collection)
         }.show(requireActivity().supportFragmentManager, ImportCollectionDialog::class.simpleName)
     }
 
-    private fun createCollectionDialog() {
-        hideActionButtonMenu()
+    private fun newCollectionDialog() {
         NameCollectionDialog { name ->
             viewModel.createCollection(requireContext(), name)
         }.show(requireActivity().supportFragmentManager, NameCollectionDialog::class.simpleName)
---
+++ app/src/main/java/com/andlill/jld/app/main/dialog/CollectionOptionsDialog.kt
@@ -0,0 +1,36 @@
+package com.andlill.jld.app.main.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.andlill.jld.R
+import com.andlill.jld.utils.AppUtils
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class CollectionOptionsDialog(private val callback: (Action) -> Unit) : BottomSheetDialogFragment()  {
+
+    enum class Action {
+        Import,
+        New,
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val layout = layoutInflater.inflate(R.layout.dialog_collection_options, container, false)
+
+        layout.findViewById<View>(R.id.button_import).setOnClickListener {
+            AppUtils.postDelayed(150) {
+                callback(Action.Import)
+                dismiss()
+            }
+        }
+        layout.findViewById<View>(R.id.button_new).setOnClickListener {
+            AppUtils.postDelayed(150) {
+                callback(Action.New)
+                dismiss()
+            }
+        }
+
+        return layout
+    }
+}
