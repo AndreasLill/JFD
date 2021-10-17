0e7ca6c3fafa67eb95f90f70c5e278a73271aff5
Andreas <andreas.ste.lill@gmail.com>
2021-10-17 15:59:18 +0200

    Added action snackbar to "add to collection" dialog.

--- app/src/main/java/com/andlill/jld/app/dictionarydetails/dialog/AddToCollectionDialog.kt
+++ app/src/main/java/com/andlill/jld/app/dictionarydetails/dialog/AddToCollectionDialog.kt
@@ -2,20 +2,18 @@ package com.andlill.jld.app.dictionarydetails.dialog
 
 import android.app.Dialog
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
 import androidx.appcompat.app.AlertDialog
-import androidx.core.os.HandlerCompat
 import androidx.fragment.app.DialogFragment
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.andlill.jld.R
-import com.andlill.jld.app.dictionarydetails.adapter.AddToCollectionAdapter
 import com.andlill.jld.app.dictionarydetails.DictionaryDetailsViewModel
+import com.andlill.jld.app.dictionarydetails.adapter.AddToCollectionAdapter
 import com.andlill.jld.model.Collection
 import com.andlill.jld.model.DictionaryEntry
 import com.andlill.jld.utils.AppUtils
+import com.google.android.material.snackbar.Snackbar
 
 class AddToCollectionDialog(private val viewModel: DictionaryDetailsViewModel) : DialogFragment() {
 
@@ -31,9 +29,11 @@ class AddToCollectionDialog(private val viewModel: DictionaryDetailsViewModel) :
             adapter = AddToCollectionAdapter(collections, entry) { collection ->
                 if (!collection.content.contains(entry.id)) {
                     collection.content.add(entry.id)
+                    Snackbar.make(requireActivity().findViewById(R.id.layout_root), String.format(getString(R.string.dictionary_add_to_collection), entry.getReading(), collection.name), 1000).show()
                 }
                 else {
                     collection.content.remove(entry.id)
+                    Snackbar.make(requireActivity().findViewById(R.id.layout_root), String.format(getString(R.string.dictionary_remove_from_collection), entry.getReading(), collection.name), 1000).show()
                 }
 
                 viewModel.updateCollection(requireContext(), collection)
